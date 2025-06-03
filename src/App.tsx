e60b025ae4222fba413162500280efb8ce8e6b70
Raj Kashyap <rajkashyapatservice@outlook.com>
2025-06-03 19:40:57 +0530

    Removed Loader and Reduced The Image Loading Time

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,9 @@ import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-import Loader from "./components/Loader/Loader";
-import { useEffect, useState } from "react";
 const App = () => {
-  const [loaded, setLoaded] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoaded(!loaded);
-    }, 4000);
-  }, []);
-
   return (
     <>
-      {loaded && <Loader loaded={loaded} />}
       <Navbar />
       <Hero />
       <About />
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,17 +1,38 @@
 import "./Hero.css";
+import { motion } from "framer-motion";
 
 const Hero: React.FC = () => {
+  const fadeInAnimation = {
+    hidden: { opacity: 0, y: 75 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
-    <section id="Home" className="hero">
+    <motion.section
+      id="Home"
+      className="hero"
+      variants={fadeInAnimation}
+      initial="hidden"
+      animate="visible"
+      transition={{ duration: 0.5, delay: 0.25 }}
+    >
       <div className="hero-content flow">
-        <h1 className="hero-title">
+        <motion.h1
+          className="hero-title"
+          variants={fadeInAnimation}
+          transition={{ duration: 0.5 }}
+        >
           Hi, I’m <span className="text-gradient">Raj</span>
-        </h1>
-        <p className="hero-text">
+        </motion.h1>
+        <motion.p
+          className="hero-text"
+          variants={fadeInAnimation}
+          transition={{ duration: 0.5, delay: 0.25 }}
+        >
           I’m a passionate and versatile Full Stack Developer with expertise in
           web development, AI integration, and modern design principles. With a
           strong foundation in both front-end and back-end technologies.
-        </p>
+        </motion.p>
 
         <a
           href="/portfolio/docs/resume.docx"
@@ -22,9 +43,9 @@ const Hero: React.FC = () => {
         </a>
       </div>
       <div className="hero-image">
-        <img src="/portfolio/images/hero.png" alt="Hero Image" />
+        <img src="/portfolio/images/hero.webp" alt="Hero Image" />
       </div>
-    </section>
+    </motion.section>
   );
 };
 
